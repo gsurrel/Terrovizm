cfdb9d280e0d1f8ae59113c65c64d0c2eb3c46bd
surrel <gregoire.surrel@epfl.ch>
2017-11-28 15:25:06 +0100

    Custom markers with small piecharts (WIP, just displaying white text now)

--- docs/terrovizm.js
+++ docs/terrovizm.js
@@ -59,10 +59,67 @@ function reformatData(json) {
         return x;
     });
 
-    // Fill-in the data crossfiltered
+    // Transform data from unlabled array to an object
     let eventKeys = Object.keys(columns);
     data.events = json.events.map(x => _.object(eventKeys, x));
+
+    // Create the Leaflet markers once and for all
+    data.events.map(function(x) {
+        x["marker"] = new PruneCluster.Marker(
+            x.latitude,
+            x.longitude,
+            {
+                "icon": function(data, category){
+                    // Pie chart for markers
+                    /*console.log(`<span>${x.nkill} ${x.nwound}</span>`);
+                    let span = document.createElement("span");
+                    let length = 32, radius = length/2;
+                    var arc = d3.svg.arc()
+                        .outerRadius(radius - 10)
+                        .innerRadius(0);
+                    let pie = d3.layout.pie()
+                        .sort(null)
+                        .value(d => ({nkill: x.nkill, nwound: x.nwound}));
+                    let g = d3.select(span)
+                        .append("svg")
+                        .attr("width", length)
+                        .attr("height", length)
+                        .append("g")
+                        .attr("transform", "translate(" + length / 2 + "," + length / 2 + ")");
+                    g.append("path")
+                        .data(d => ({nkill: x.nkill, nwound: x.nwound}))
+                        .attr("d", arc)
+                        .style("fill", "red")*/
+                    return new L.divIcon({html: `<span>${x.nkill} ${x.nwound}</span>`});
+                },
+                "popup": `<h2>${(new Date(x.date)).toDateString()}</h2>
+                <h3>Casualties</h3>
+                <ul>
+                    <li>Killed: ${x.nkill}</li>
+                    <li>Wounded: ${x.nwound}</li>
+                    <li>Suicide: ${x.suicide ? "yes" : "no"}</li>
+                </ul>
+                <h3>Target(s)</h3>
+                <ul>
+                    ${x.targtype.reduce((acc, y) => `<li>${data.refs.targtype[y]}</li>`, "")}
+                </ul>
+                <h3>Weapons(s)</h3>
+                <ul>
+                    ${x.weaptype.reduce((acc, y) => `<li>${data.refs.weaptype[y]}</li>`, "")}
+                </ul>
+                <h3>More…</h3>
+                <a href="http://www.start.umd.edu/gtd/search/IncidentSummary.aspx?gtdid=${x.eventid}" target="_blank">Details</a>`
+            },
+            null,
+            true);
+        map.pruneCluster.RegisterMarker(x.marker);
+        return x;
+    });
+
+    // Fill-in the data crossfiltered
     xf = crossfilter(data.events);
+    xf.lat = xf.dimension(x => x.latitude),
+    xf.lon = xf.dimension(x => x.longitude);
 
     // Log
     reformatTime = new Date();
@@ -74,35 +131,8 @@ function loadMap() {
     console.log("Start load markers in map");
     let t1 = new Date();
 
-    let lat = xf.dimension(x => x.latitude),
-        lon = xf.dimension(x => x.longitude);
-
-    let topLats = lat.top(Infinity);
-
-    let pruneCluster = new PruneClusterForLeaflet(100, 20);
-    let markers = topLats.map(x => new PruneCluster.Marker(
-        x.latitude,
-        x.longitude,
-        {"popup": `<h2>${(new Date(x.date)).toDateString()}</h2>
-        <h3>Casualties</h3>
-        <ul>
-            <li>Killed: ${x.nkill}</li>
-            <li>Wounded: ${x.nwound}</li>
-            <li>Suicide: ${x.suicide ? "yes" : "no"}</li>
-        </ul>
-        <h3>Target(s)</h3>
-        <ul>
-            ${x.targtype.reduce((acc, y) => `<li>${data.refs.targtype[y]}</li>`, "")}
-        </ul>
-        <h3>Weapons(s)</h3>
-        <ul>
-            ${x.weaptype.reduce((acc, y) => `<li>${data.refs.weaptype[y]}</li>`, "")}
-        </ul>
-        <h3>More…</h3>
-        <a href="http://www.start.umd.edu/gtd/search/IncidentSummary.aspx?gtdid=${x.eventid}" target="_blank">Details</a>`},
-        x.country));
-    markers.forEach(x => pruneCluster.RegisterMarker(x));
-    map.addLayer(pruneCluster);
+    //let topLats = xf.lat.top(5);
+    map.pruneCluster.RedrawIcons();
 
     // Log
     let t2 = new Date();
@@ -119,4 +149,21 @@ function setupMap() {
     L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
         attribution: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    // Add the map filtering
+    let locationFilter = new L.LocationFilter().addTo(map);
+    locationFilter.on("change", function (e) {
+        console.log("Bounds changed", e.bounds);
+
+    });
+    locationFilter.on("enabled", function () {
+        console.log("Geofilter enabled");
+    });
+    locationFilter.on("disabled", function () {
+        console.log("Geofilter disabled");
+    });
+
+    // Create and add the PruneCluster layer
+    map["pruneCluster"] = new PruneClusterForLeaflet(100, 20);
+    map.addLayer(map.pruneCluster);
 }
