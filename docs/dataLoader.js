5add5c68da1b1bdaa2378f9cb472ff73b2ae8759
gsurrel <gregoire+github@surrel.org>
2017-11-30 23:36:28 +0100

    Map made into class, data loading in background thread

---
+++ docs/dataLoader.js
@@ -0,0 +1,80 @@
+"use strict"
+
+// Performance timers
+let startTime;
+let downloadTime;
+let reformatTime;
+let transmissionTime;
+let data = {};
+
+onmessage = function(e) {
+    console.log("Loading data", e.data.dateString);
+    startTime = new Date();
+
+    importScripts(...e.data.imports);
+
+    d3.json(`db-${e.data.dateString}.json`, function(error, json) {
+        // Log
+        if (error) return console.error(error);
+        downloadTime = new Date();
+        console.log("Download and parsing finished", downloadTime - startTime);
+
+        // Process data
+        reformatData(json);
+
+        // Kill worker to free memory
+        close();
+    });
+}
+
+function reformatData(json) {
+    // We did not use a dictionnary for the events to save network space.
+    // Let's recreate a nice data structure.
+
+    // Split the columns names apart, then all the refs are fine
+    let columns = json.refs.columns;
+    delete json.refs.columns;
+    data.refs = json.refs;
+
+    // Set unknown days and months to 1st day and month
+    json.events.map(function(x) {
+        x[columns.date].filter(y => y == 0).map(y => y = 1);
+        x[columns.date][1]--; // Months start at 0 and not 1
+        return x;
+    });
+
+    // Create the date in the iyear field, which we rename
+    json.events.map(function(x) {
+        x[columns.date] = Date.UTC(...x[columns.date]);
+        return x;
+    });
+
+    // Transform data from unlabled array to an object
+    let eventKeys = Object.keys(columns);
+    data.events = json.events.map(x => _.object(eventKeys, x));
+
+    // Create the Leaflet markers once and for all
+    data.events.map(function(x) {
+        x["marker"] = [
+            x.latitude,
+            x.longitude,
+            {
+                "nkill": x.nkill,
+                "nwound": x.nwound,
+                "icon": (() => undefined),
+                "popup": (() => undefined)
+            },
+            null,
+            true];
+        return x;
+    });
+
+    // Log processing time
+    reformatTime = new Date();
+    console.log("Reformatting of data finished", reformatTime - downloadTime);
+
+    // Log transmission time
+    postMessage(JSON.stringify(data));
+    transmissionTime = new Date();
+    console.log("Transmitting data to main thread finished", transmissionTime - reformatTime);
+}
--- docs/terrovizm.js
+++ docs/terrovizm.js
@@ -8,79 +8,8 @@ let xf;
 // Ready flag
 let ready = false;
 
-// Performance timers
-let startTime = new Date();
-let downloadTime;
-let reformatTime;
-
-// References to UI objects
-let map;
-
-function createrMarkerText(event) {
-    return `<h2>${(new Date(event.date)).toDateString()}</h2>
-    <h3>Casualties</h3>
-    <ul>
-        <li>Killed: ${event.nkill}</li>
-        <li>Wounded: ${event.nwound}</li>
-        <li>Suicide: ${event.suicide ? "yes" : "no"}</li>
-    </ul>
-    <h3>Target(s)</h3>
-    <ul>
-        ${event.targtype.reduce((acc, y) => `<li>${data.refs.targtype[y]}</li>`, "")}
-    </ul>
-    <h3>Weapons(s)</h3>
-    <ul>
-        ${event.weaptype.reduce((acc, y) => `<li>${data.refs.weaptype[y]}</li>`, "")}
-    </ul>
-    <h3>More…</h3>
-    <a href="http://www.start.umd.edu/gtd/search/IncidentSummary.aspx?gtdid=${event.eventid}" target="_blank">Details</a>`
-}
-
-function createMarkerPie(nkill, nwound, markersize, clustersize, nocasualties) {
-    let len = markersize;
-    let svg = d3.select(document.createElement("div"))
-    //let svg = d3.select("body")
-        .append("svg")
-        .attr("width", len)
-        .attr("height", len);
-    if(nkill + nwound != 0) {
-        svg.append("path")
-            .attr("d", d3.svg.arc()
-                .innerRadius(0)
-                .outerRadius(len/2)
-                .startAngle(0)
-                .endAngle(-nkill / (nwound + nkill) * Math.PI * 2 * (clustersize === void 0 ? 1 : (1-nocasualties/clustersize))))
-            .attr("fill", "red")
-            .attr("transform", `translate(${len/2},${len/2})`);
-        svg.append("path")
-            .attr("d", d3.svg.arc()
-                .innerRadius(0)
-                .outerRadius(len/2)
-                .startAngle(0)
-                .endAngle(nwound / (nwound + nkill) * Math.PI * 2 * (clustersize === void 0 ? 1 : (1-nocasualties/clustersize))))
-            .attr("fill", "orange")
-            .attr("transform", `translate(${len/2},${len/2})`);
-    }
-    let circle = svg.append("circle")
-        .attr("r", len*2/5)
-        .attr("cx", len/2)
-        .attr("cy", len/2)
-        .attr("fill", clustersize === void 0 ? "white" : "#ddd");
-    if(nkill + nwound == 0) {
-        circle.attr("stroke", "black");
-    }
-    if (clustersize !== void 0) {
-        svg.append("text")
-            .attr("x", len/2)
-            .attr("y", len/2)
-            .attr("dy", 5)
-            .attr("text-anchor", "middle")
-            .attr("fill", "black")
-            .text(clustersize);
-    }
-
-    return svg;
-}
+// References to map class instance
+let mapT;
 
 function updateLoader() {
     let strings = ["Downloading terrorists",
@@ -105,139 +34,46 @@ function main() {
         .text("Terr💣vizm");
 
     updateLoader();
+    mapT = new TerroMap();
 
-    setupMap();
+    let dataLoader = new Worker('dataLoader.js');
+    dataLoader.postMessage({"dateString": "2017-11-25", "imports": ["d3.v3.min.js", "underscore.min.js"]});
+    dataLoader.onmessage = function(e) {
+        let t1 = new Date();
+        console.log("Transmitting data from worker finished");
+        // Save the data sent from the worker
+        let dataWorker = JSON.parse(e.data);
+        data["refs"] = dataWorker.refs;
 
-    let dateString = "2017-11-25";
-    console.log("Loading data", dateString);
+        let t2 = new Date();
+        console.log("Parsed data from worker", t2-t1);
 
-    d3.json(`db-${dateString}.json`, function(error, json) {
-        // Log
-        if (error) return console.error(error);
-        downloadTime = new Date();
-        console.log("Download and parsing finished", `Time ${(downloadTime - startTime)/1000} seconds`);
+        // Complete the icon marker generation
+        dataWorker.events.map(function(x) {
+            // Create the marker icon in the right spot before destructuring
+            x.marker[2].icon = (() => new L.divIcon({
+                    html: TerroMap.createMarkerPie(x.nkill, x.nwound, 32).node().outerHTML,
+                    iconAnchor: [16, 16],
+                    className: "killwoundmarker"
+                }));
+            x.marker[2].popup = (() => TerroMap.createrMarkerText(x));
 
-        // Process data
-        reformatData(json);
-
-        // Load data on the map
-        loadMap();
-    });
-
-    ready = true;
-}
-
-function reformatData(json) {
-    // We did not use a dictionnary for the events to save network space.
-    // Let's recreate a nice data structure.
-
-    // Split the columns names apart, then all the refs are fine
-    let columns = json.refs.columns;
-    delete json.refs.columns;
-    data.refs = json.refs;
-
-    // Set unknown days and months to 1st day and month
-    json.events.map(function(x) {
-        x[columns.date].filter(y => y == 0).map(y => y = 1);
-        x[columns.date][1]--; // Months start at 0 and not 1
-        return x;
-    });
-
-    // Create the date in the iyear field, which we rename
-    json.events.map(function(x) {
-        x[columns.date] = Date.UTC(...x[columns.date]);
-        return x;
-    });
-
-    // Transform data from unlabled array to an object
-    let eventKeys = Object.keys(columns);
-    data.events = json.events.map(x => _.object(eventKeys, x));
-
-    // Create the Leaflet markers once and for all
-    data.events.map(function(x) {
-        x["marker"] = new PruneCluster.Marker(
-            x.latitude,
-            x.longitude,
-            {
-                "nkill": x.nkill,
-                "nwound": x.nwound,
-                "icon": function(data, category){
-                    return new L.divIcon({
-                        html: createMarkerPie(x.nkill, x.nwound, 32).node().outerHTML,
-                        iconAnchor: [16, 16],
-                        className: "killwoundmarker"
-                    });
-                },
-                "popup": (() => createrMarkerText(x))
-            },
-            null,
-            true);
-        map.pruneCluster.RegisterMarker(x.marker);
-        return x;
-    });
-
-    // Fill-in the data crossfiltered
-    xf = crossfilter(data.events);
-    xf.lat = xf.dimension(x => x.latitude),
-    xf.lon = xf.dimension(x => x.longitude);
-
-    // Log
-    reformatTime = new Date();
-    console.log("Reformatting of data finished", `Time ${(reformatTime - downloadTime)/1000} seconds`);
-}
-
-function loadMap() {
-    // Log
-    console.log("Start load markers in map");
-    let t1 = new Date();
-
-    //let topLats = xf.lat.top(5);
-    map.pruneCluster.RedrawIcons();
-
-    // Log
-    let t2 = new Date();
-    console.log("Time in ms: ", t2-t1);
-}
-
-function setupMap() {
-    d3.select("body")
-        .append("div")
-        .attr("id","map");
-
-    map = L.map('map').setView([0, 0], 2);
-
-    L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
-        attribution: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // Add the map filtering
-    let locationFilter = new L.LocationFilter().addTo(map);
-    locationFilter.on("change", function (e) {
-        console.log("Bounds changed", e.bounds);
-
-    });
-    locationFilter.on("enabled", function () {
-        console.log("Geofilter enabled");
-    });
-    locationFilter.on("disabled", function () {
-        console.log("Geofilter disabled");
-    });
-
-    // Create and add the PruneCluster layer
-    map["pruneCluster"] = new PruneClusterForLeaflet(100, 20);
-    PruneCluster.Cluster.ENABLE_MARKERS_LIST = true
-    map.pruneCluster.BuildLeafletClusterIcon = function(cluster) {
-        let markers = cluster.GetClusterMarkers();
-        let nkill = markers.reduce((acc, x) => acc + x.data.nkill, 0);
-        let nwound = markers.reduce((acc, x) => acc + x.data.nwound, 0);
-        let nocasualties = markers.reduce((acc, x) => acc + x.data.nkill == 0 & x.data.nwound == 0, 0);
-        let iconSize = 24 + Math.sqrt(cluster.population/80);
-        return new L.divIcon({
-            html: createMarkerPie(nkill, nwound, iconSize, cluster.population, nocasualties).node().outerHTML,
-            iconAnchor: [iconSize/2, iconSize/2],
-            className: "killwoundmarker"
+            // Create markers in the PruneCluser destructuring the returned data
+            x.marker = new PruneCluster.Marker(...x.marker);
+            mapT.pruneCluster.RegisterMarker(x.marker);
+            return x;
         });
-    };
-    map.addLayer(map.pruneCluster);
 
+        let t3 = new Date();
+        console.log("Instantiated marker data", t3-t2);
+
+        // Fill-in the data crossfiltered
+        xf = crossfilter(dataWorker.events);
+        xf.lat = xf.dimension(x => x.latitude),
+        xf.lon = xf.dimension(x => x.longitude);
+
+        mapT.refreshMarkers();
+
+        ready = true;
+    }
 }
