3ef3735e4c0deb68a23634c028f7e2a0b51edc51
surrel <gregoire+github@surrel.org>
2017-12-12 11:24:21 +0100

    Close #20

--- docs/terrovizm.js
+++ docs/terrovizm.js
@@ -5,8 +5,6 @@
 let data = new Object();
 let xf;
 
-let victimsSummaries = undefined;
-
 // Ready flag
 let ready = false;
 
@@ -38,11 +36,11 @@ function main() {
             // Create the marker icon in the right spot before destructuring
             let markerSize = TerroMap.markerSize(x.nkill+x.nwound);
             x.marker[2].icon = (() => new L.divIcon({
-                    html: TerroMap.createMarkerPie(x.nkill, x.nwound, (x.nkill+x.nwound)==0,
-                        markerSize).node().outerHTML,
-                    iconAnchor: [markerSize/2, markerSize/2],
-                    className: "killwoundmarker"
-                }));
+                html: TerroMap.createMarkerPie(x.nkill, x.nwound, (x.nkill+x.nwound)==0,
+                markerSize).node().outerHTML,
+                iconAnchor: [markerSize/2, markerSize/2],
+                className: "killwoundmarker"
+            }));
             x.marker[2].popup = (() => TerroMap.createrMarkerText(x));
 
             // Create markers in the PruneCluser destructuring the returned data
@@ -80,19 +78,31 @@ function refreshView(){
     let b = mapT.map.getBounds();
     let markers = mapT.pruneCluster.Cluster.FindMarkersInArea(
         {"minLat": b.getSouth(),
-         "maxLat": b.getNorth(),
-         "minLng": b.getWest(),
-         "maxLng": b.getEast()
-     });
-     alert(markers.length);
-    // update the summaries
-    d3.select('#selected-events-details')
-        .html(`#killed <strong>${victimsSummaries.value()['nkill'].toLocaleString()}</strong> - #wounded <strong>${victimsSummaries.value()['nwound'].toLocaleString()}</strong>`)
-}
+        "maxLat": b.getNorth(),
+        "minLng": b.getWest(),
+        "maxLng": b.getEast()
+    });
+    let markersInView = markers.reduce((acc, x) => acc + !(x.filtered), 0);
+    if(markersInView == 0) {
+        let markersOnMap = mapT.pruneCluster.GetMarkers().reduce((acc, x) => acc + !(x.filtered), 0);
+        if(markersOnMap != 0) {
+            let zoomToMarkers = window.confirm('No attacks in this area. Show the attacks?');
+            if(zoomToMarkers) {
+                let minLat = xf.lat.bottom(1)[0].latitude;
+                let maxLat = xf.lat.top(1)[0].latitude;
+                let minLng = xf.lon.bottom(1)[0].longitude;
+                let maxLng = xf.lon.top(1)[0].longitude;
+                mapT.map.flyToBounds(L.latLngBounds(
+                    L.latLng(minLat, minLng),
+                    L.latLng(maxLat, maxLng)));
+                }
+            }
+        }
+    }
 
-function createSummaries(){
-    let all = xf.groupAll();
-    let evCount = dc.dataCount('#selected-events')
+    function createSummaries(){
+        let all = xf.groupAll();
+        let evCount = dc.dataCount('#selected-events')
         .dimension(xf)
         .group(all)
         .html({
@@ -100,26 +110,10 @@ function createSummaries(){
             '<span class="reset" onclick="javascript:dc.filterAll(); dc.renderAll();">Reset all</span>',
             all: 'All records selected. Please click on bar charts or select a time range to apply filters.'
         });
+    }
 
-    victimsSummaries = xf.groupAll();
-    victimsSummaries = victimsSummaries.reduce(
-            (p,v) => {
-                p['nkill'] += v['nkill'];
-                p['nwound'] += v['nwound'];
-                return p;
-            },
-            (p,v) => {
-                p['nkill'] -= v['nkill'];
-                p['nwound'] -= v['nwound'];
-                return p;
-            },
-            () =>{return {nkill:0, nwound:0};}
-        );
-
-}
-
-function updateLoader() {
-    let strings = ["Downloading terrorists",
+    function updateLoader() {
+        let strings = ["Downloading terrorists",
         "Planning evil plots",
         "Building master plan",
         "Contacting law enforcment",
